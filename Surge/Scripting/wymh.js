f4d94447b98833152f63c516bccee6c77e24ed0f
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-05-17 12:35:29 +0800

    Create wymh.js

---
+++ Surge/Scripting/wymh.js
@@ -0,0 +1,18 @@
+let path = '/getUserProfile';
+var obj = JSON.parse(body)
+if (url.indexOf(path) != -1) {
+	obj["resCode"] = 0;
+	obj["data"]["money"] = 55555;
+	obj["data"]["hongbao"] = 6669;
+	obj["data"]["realVipType"] = 1;
+	obj["data"]["score"] = 9996;
+    obj["data"]["userType"] = 1;
+    obj["data"]["level"] = "VIP8";
+    obj["data"]["yuepiao"] = 666;
+    obj["data"]["to"] = 7262649741721;
+    obj["data"]["from"] = 1543574359721;
+ }
+ 
+ // Made by Meeta (网易漫画）
+ // https?:\/\/api\.mh\.163.com\/getUserProfile
+ // hostname = api.mh.163.com
