8b3192b871c2b55e61d108a749e6cbc4e358915b
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-05-19 17:18:44 +0800

    Create watermelonvideo.js

---
+++ Surge/Scripting/watermelonvideo.js
@@ -0,0 +1,25 @@
+var result = body
+let path1 = "/api/user/get";
+let path2 = "/api/user/getAdv";
+if (url.indexOf(path1) != -1){
+  var obj = JSON.parse(body);
+  obj.content["userName"] = "Meeta123456";
+  obj.content["level"] = "5";
+  obj.content["nickName"] = "Mee****456";
+  obj.content["vipType"] = "1";
+  result = JSON.stringify(obj);
+}
+if (url.indexOf(path2) != -1){
+  var obj = JSON.parse(body);
+  obj.content["img"] = "http://ww1.sinaimg.cn/large/0076dY5Wgy1g36mmbdvv7j30gf0zkwf2.jpg";
+  obj.content["url"] = "";
+  result = JSON.stringify(obj);
+}
+  
+
+result;
+
+/* Made by Meeta(西瓜视频：www.xg1.app)
+https?:\/\/api\.gkaorlz\.com\/api\/user\/get
+hostname = api.gkaorlz.com
+*/
