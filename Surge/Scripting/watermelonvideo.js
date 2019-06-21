9737d512a7f01020e0e8a4036a56361e583e8bef
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-06-21 16:29:44 +0800

    Update watermelonvideo.js

--- Surge/Scripting/watermelonvideo.js
+++ Surge/Scripting/watermelonvideo.js
@@ -1,21 +1,21 @@
-var result = body
-let path1 = "/api/user/get";
-let path2 = "/api/user/getAdv";
+const path1 = "/api/user/get";
+const path2 = "/api/user/getAdv";
+var body = $response.body;
+var url = $request.url;
 if (url.indexOf(path1) != -1){
-  var obj = JSON.parse(body);
+  let obj = JSON.parse(body);
   obj.content["level"] = "5";
   obj.content["vipType"] = "1";
-  result = JSON.stringify(obj);
+  body = JSON.stringify(obj);
 }
 if (url.indexOf(path2) != -1){
-  var obj = JSON.parse(body);
+  let obj = JSON.parse(body);
   obj.content["img"] = "http://ww1.sinaimg.cn/large/0076dY5Wgy1g36mmbdvv7j30gf0zkwf2.jpg";
-  obj.content["url"] = "https://raw.githubusercontent.com/MeetaGit/MeetaRules/master/README.md";
-  result = JSON.stringify(obj);
+  obj.content["url"] = "";
+  body = JSON.stringify(obj);
 }
   
-
-result;
+$done({body});
 
 /* Made by Meeta(西瓜视频：www.xg1.app)
 https?:\/\/api\.gkaorlz\.com\/api\/user\/get
