7d8f7d9d9453190e439691afbdb84b8e96141510
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-06-22 13:19:15 +0800

    Update kuwovip.js

--- Surge/Scripting/kuwovip.js
+++ Surge/Scripting/kuwovip.js
@@ -3,12 +3,12 @@ var url = $request.url;
 const path1 = "kuwo.cn/AdService/kaiping/";
 const path2 = "kuwo.cn/vip/v2/user/vip";
 if (url.indexOf(path1) != -1){
-	let obj = JSON.parse(body);
+	var obj = JSON.parse(body);
 	obj.url = "http://ww1.sinaimg.cn/large/0076dY5Wgy1g36mmbdvv7j30gf0zkwf2.jpg";
 body = JSON.stringify(obj);
 }
 if (url.indexOf(path2) != -1){
-	let obj = JSON.parse(body);
+	var obj = JSON.parse(body);
 	obj.data["isNewUser"] = "2";
 	obj.data["vipLuxuryExpire"] = "1835312949000";
 	obj.data["time"] = "1961170340993";
