8df01b950f18e63dfc2235aa316b3b1e659e75ea
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-06-24 17:33:36 +0800

    Update hourlyWeather.js

--- Surge/Scripting/hourlyWeather.js
+++ Surge/Scripting/hourlyWeather.js
@@ -22,6 +22,6 @@ $httpClient.get(weaapi, function(error, response, data){
 cron "* * * *" script-path=https://meetagit.github.io/MeetaRules/Surge/Scripting/hourlyWeather.js
 天气预报每小时通过通知栏提醒一次（可以自己修改提醒频率)；
 向通知中心发送通知，Surge iOS 上需开启通知总开关；
-欢迎大家反馈
+欢迎大家使用
 欢迎关注TG频道:@meetashare
 */
