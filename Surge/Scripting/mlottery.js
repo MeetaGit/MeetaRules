bd90e03c1a103b8c5894b67219a5fc1fee607782
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-07-08 22:46:00 +0800

    Update mlottery.js

--- Surge/Scripting/mlottery.js
+++ Surge/Scripting/mlottery.js
@@ -1,3 +1,19 @@
+/*
+彩票助手（Made by Meeta)
+
+
+cron "* * 22 * * *" script-path=https://meetagit.github.io/MeetaRules/Surge/Scripting/mlottery.js
+
+向通知中心发送通知，Surge iOS 上需开启通知总开关；
+欢迎大家使用,要中大奖哟！
+欢迎关注TG频道:@meetashare
+*/
+
+
+
+const k = ["qg1.ashx?lotCode=10039","qg1.ashx?lotCode=10040","qg2.ashx?lotCode=10041"];
+
+
 for(var i=0;i<k.length;i++){
 	var cpapi = "http://api.1888kuai.com/"+k[i];
 	$httpClient.get(cpapi, function(error, response, data){
@@ -20,15 +36,4 @@ for(var i=0;i<k.length;i++){
 		)
 }
 
-
-/*
-彩票助手（Made by Meeta)
-
-
-cron "* * 22 * * *" script-path=https://meetagit.github.io/MeetaRules/Surge/Scripting/mlottery.js
-
-向通知中心发送通知，Surge iOS 上需开启通知总开关；
-欢迎大家使用,要中大奖哟！
-欢迎关注TG频道:@meetashare
-*/
 
