d454d8a51e0c96a07b2516f768026cf638e4cac9
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-07-11 21:29:35 +0800

    Update cushawmovie.js

--- Surge/Scripting/cushawmovie.js
+++ Surge/Scripting/cushawmovie.js
@@ -3,6 +3,7 @@
 1.文本编辑模式下复制粘贴
 http-response https?:\/\/p\.doras\.api\.vcinema\.cn\/v5.0\/user/  script-path=https://meetagit.github.io/MeetaRules/Surge/Scripting/cushawmovie.js,requires-body=true
 2.添加hostname = *.api.vcinema.cn
+3.南瓜电影重新登录账号
 欢迎关注TG频道:@meetashare
 */
 
