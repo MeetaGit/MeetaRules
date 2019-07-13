35c7707d718e4eba932c4d1b8278c901440d3c8f
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-07-13 22:32:35 +0800

    Update smallvideo.js

--- Surge/Scripting/smallvideo.js
+++ Surge/Scripting/smallvideo.js
@@ -15,8 +15,9 @@ var body = $response.body;
 var url = $request.url;
 if (url.indexOf(path1) != -1){
 	let obj = JSON.parse(body);
-	obj.data.uinfo["down_daily_remainders"] = "888";
-	obj.data.uinfo["play_daily_remainders"] = "888";
+	obj.data.uinfo["down_daily_remainders"] = "988";
+	obj.data.uinfo["play_daily_remainders"] = "988";
+	obj.data.uinfo["goldcoin"] = "999";
 	obj.data.uinfo["next_upgrade_need"] = "0";
 	obj.data.uinfo.curr_group["gicon"] = "V5";
     obj.data.uinfo.curr_group["gid"] = "5";
@@ -25,14 +26,18 @@ if (url.indexOf(path1) != -1){
 	obj.data.user["nickname"] = "Meeta_share";
 	obj.data.user["avatar_url"] = "https://img.sdxaly.com/C1/avatar/044/44414.jpeg?t=1563021092";
 	obj.data.user["avatar"] = "044/44414.jpeg?t=1563021092"
+	obj.data.user["goldcicon"] = "998";
+	obj.data.user["gicon"] = "V5";
 	body = JSON.stringify(obj);
 }
 if (url.indexOf(path2) != -1){
   let obj = JSON.parse(body);
   obj.retcode = "0";
   obj.data.lastplayindex = "1";
-  obj.data["httpurl_preview"] = "httpurl";
+  obj.data["httpurl"] = obj.data["httpurl_preview"];
+  delete obj.data["httpurl_preview"];
   body = JSON.stringify(obj);
 }
 
 $done({body});
+
