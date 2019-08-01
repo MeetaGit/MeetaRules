5cd68cba1984b46b59a9d7a4830627e0d25d61cb
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-08-01 22:57:18 +0800

    Update lottery_xyft.js

--- Surge/Custom_Js/lottery_xyft.js
+++ Surge/Custom_Js/lottery_xyft.js
@@ -1,4 +1,7 @@
-// Made for @zaxbaby
+// 幸运飞艇（Made for @zaxbaby）
+// 该api 由  @zaxbaby 私人购买,免费分享,所以请大家不要滥用
+// ps :未经允许不允许以此谋利
+// TG频道:@meetashare
 
 const api = "http://vip.manycai.com/K25d41658517f18/xyft-1.json";
 $httpClient.get(api, function(error, response, data){
