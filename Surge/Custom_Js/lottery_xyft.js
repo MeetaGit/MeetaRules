ffdde2f192ab93af0505b45443c6fd3089aa50bc
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-08-01 21:16:27 +0800

    Update and rename lottery_@zaxbaby.js to lottery_xyft.js

--- Surge/Custom_Js/lottery_xyft.js
+++ Surge/Custom_Js/lottery_xyft.js
@@ -1,4 +1,4 @@
-// Made for @zaxbaby.js
+// Made for @zaxbaby
 
 const api = "http://vip.manycai.com/K25d41658517f18/xyft-1.json";
 $httpClient.get(api, function(error, response, data){
@@ -8,10 +8,10 @@ $httpClient.get(api, function(error, response, data){
   }else{
     var obj = JSON.parse(data);
     //console.log(obj);
-    let name = obj[0].lotterycode;
+    //let name = obj[0].lotterycode;
     let code = obj[0].code;
     let opendate = obj[0].opendate;
-    let title = name+"-开奖信息";
+    let title = "幸运飞艇-开奖信息";
     let subtitle = "中奖号码："+code;
     let mation = "开奖时间："+opendate+"\nMeeta wish you hit the jackpot !";
     $notification.post(title, subtitle, mation);
