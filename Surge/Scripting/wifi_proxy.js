4ccb54222866f566cf69fb0dcb1e7870b6bb0a70
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-10-01 19:55:38 +0800

    Update wifi_proxy.js

--- Surge/Scripting/wifi_proxy.js
+++ Surge/Scripting/wifi_proxy.js
@@ -1,7 +1,13 @@
 /* wifi_proxy change (Made by Meeta)
-文本编辑模式下复制粘贴,TG频道已发教程视频
-PS:记得自己修改WIFI名称
+文本编辑模式下复制粘贴,TG频道已发操作教程视频
+
 event network-changed debug=1,script-path=wifi_proxy.js
+
+PS:记得自己修改WIFI名称
+虽然设置SSID可以达到基本相同功能
+使用脚本,Surge不会被suspend
+Rewrite和Scripting依然有效
+
 TG频道:@meetashare
 */
 
