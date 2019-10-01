f7c2afc30fcce170e06dfd24724e6621068bf910
Meeta <41709488+MeetaGit@users.noreply.github.com>
2019-10-01 22:09:36 +0800

    Update wifi_proxy.js

--- Surge/Scripting/wifi_proxy.js
+++ Surge/Scripting/wifi_proxy.js
@@ -4,6 +4,7 @@
 event network-changed debug=1,script-path=wifi_proxy.js
 
 PS:记得自己修改WIFI名称
+主要功能:指定Wi-Fi(路由器翻)下,Surge使用直连模式,其他网络下Surge使用规则模式
 虽然设置SSID可以达到基本相同功能
 使用脚本,Surge不会被suspend
 Rewrite和Scripting依然有效
